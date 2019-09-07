1b6985b9669669de37424de8b289d9b26a040515
jonathansmirnoff <jonathan@rsk.co>
2019-09-07 16:28:43 -0300

    changed logic to validate if an user is register and didn't send a message on sendMessage function.

--- contracts/contracts/MessageBoard.sol
+++ contracts/contracts/MessageBoard.sol
@@ -8,6 +8,7 @@ contract MessageBoard {
         address _address;
         string name;
         bool isValue;
+        bool messageSent;
     }
 
     //Defining Message structure. You can see it have User type member
@@ -15,10 +16,10 @@ contract MessageBoard {
         string text;
         User _user;
     }
-    
+
     //Declaring an event
 
-    event MessageCreated(string message);
+    event MessageCreated(Message message);
 
     //Constructor
     constructor() public {
@@ -31,25 +32,31 @@ contract MessageBoard {
     // public function, which is returning boolean
     function setUsername(string memory name) public returns (bool) {
         //require() is throwing error, if the input is not true
-        //require(userExists(msg.sender) == false);
-        //msg.sender is the address, from where the signUp was called.
-        users[msg.sender] = User(msg.sender, name, true);
+        require(!userExists(msg.sender), "User is registered");
+        //msg.sender is the address.
+        users[msg.sender] = User(msg.sender, name, true, false);
         return true;
     }
 
     //8 bit unsigned integer (so no negative values).
     uint8 public messageCount;
-    
+
     //it is creating an event Message with text and user.
     function sendMessage (string memory text) public returns (bool) {
         //require() is throwing error, if the input is not true
-        //require(userExists(msg.sender));
+        require(userExists(msg.sender), "User must be register");
+        require(!userSentMessage(msg.sender), "User has already sent a message");
         messageCount++;
-        emit MessageCreated(text);
+        users[msg.sender].messageSent = true;
+        emit MessageCreated(Message(text, users[msg.sender]));
         return true;
     }
-    
+
     function userExists(address userAddress) public view returns (bool) {
         return users[userAddress].isValue;
+    }
+
+    function userSentMessage(address userAddress) public view returns (bool) {
+        return users[userAddress].messageSent;
     }
 }
